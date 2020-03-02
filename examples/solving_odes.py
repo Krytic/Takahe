correct the coalescence time calculation

examples/solving_odes.py
@@ -7,35 +7,38 @@ from takahe import BinaryStarSystemLoader as load
 Solar_Mass = 1.989e30
 
 cfg = {
-	'M1' : 1.33 * Solar_Mass, # Kilogram
-	'M2' : 1.35 * Solar_Mass, # Kilograms
-	'a0' : 3.28 * 696340, # Kilometers
+	'M1' : 1.33, # Solar masses
+	'M2' : 1.35, # Solar masses
+	'a0' : 3.28, # Solar radii
 	'e0' : 0.274 # 0 <= e < 1
 }
 
-BSS = load.from_bpass(data=cfg)
+BSS = load.from_data(data=cfg)
 
-# Specify the age of the universe as an integration span
-t_span = (12.8e9, 13.8e9)
-t_eval = np.linspace(t_span[0], t_span[1], (t_span[1] - t_span[0]) / 1000)
+# Specify the last billion years as an integration span
+t_span = (0, 3e9)
 
 plt.figure()
-t, a, e = BSS.evolve(t_eval)
+t, a, e = BSS.evolve(t_span)
 
-print(BSS.coalesce())
+plt.rcParams['axes.formatter.useoffset'] = False
 
 # Plotting the SMA
 plt.subplot(121)
 plt.plot(t, a)
-plt.xlabel("Time [yrs]")
-plt.ylabel("Semimajor Axis [km]")
+plt.xlabel("Time [ga]")
+plt.ylabel("Semimajor Axis [solar radii]")
+plt.axvline(BSS.coalescence_time(), linestyle='dashed', color='black')
+# plt.ylim(0, a[0])
 
 # Plotting the Eccentricity
 plt.subplot(122)
 plt.plot(t, e)
-plt.xlabel("Time [yrs]")
+plt.xlabel("Time [ga]")
 plt.ylabel("Eccentricity [dimensionless]")
+# plt.ylim(0, e[0])
+plt.axvline(BSS.coalescence_time(), linestyle='dashed', color='black')
 
 # Boilerplate
-plt.suptitle("B1534+12")
+plt.suptitle("Star: B1534+12")
 plt.show()

takahe/BinaryStarSystem.py
@@ -6,41 +6,42 @@ from hoki import load
 Solar_Mass = 1.989e30
 
 class BinaryStarSystem:
-	def __init__(self, primary_mass, secondary_mass, a0, e0):
-		self.m1 = primary_mass
-		self.m2 = secondary_mass
-		self.a0 = a0
-		self.e0 = e0
-		self.beta = (64/5) * (G**3*self.m1*self.m2*(self.m1+self.m2))/(c**5)
+	"""Represents a binary star system."""
 
-	def coalesce(self):
-		"""Computes the coalescence time for the BSS.
+	def __init__(self, primary_mass, secondary_mass, a0, e0):
+		self.m1 = primary_mass * Solar_Mass # Units: kg
+		self.m2 = secondary_mass * Solar_Mass # Units: kg
+		self.a0 = a0 * 696340 # Units: km
+		self.e0 = e0 # Units: dimensionless
+		self.beta = (64/5) * ((G / (1000**3))**3*self.m1*self.m2*(self.m1+self.m2))/((c/1000)**5) # units: km^4 s^-1
+
+	def coalescence_time(self):
+		"""Computes the coalescence time for the BSS in gigayears.
 		
 		Uses Eqn 10 from Nyadzani and Razzaque (https://arxiv.org/pdf/1905.06086.pdf) to compute the coalescence
 		time for the binary star system. Although this is strictly speaking an approximation, it converges on the
-		result gained by numeri cally by Peters in 1964.
+		result gained by numerically by Peters in 1964.
 		
 		Returns:
-			float -- the coalescence time for the binary star.
+			float -- the coalescence time for the binary star (units: ga).
 		"""
 
-		return self.a0**4 / (4*self.beta) * (1-self.e0**2)**(7/2) / ((1-self.e0**(7/4))**(1/5)*(1+121/304 * self.e0**2))
+		return (self.a0**4 / (4*self.beta) * (1-self.e0**2)**(7/2) / ((1-self.e0**(7/4))**(1/5)*(1+121/304 * self.e0**2))) / (31557600000000000) # Units: ga
 
-	def evolve(self, evolve_over):
+	def evolve(self, t_span):
 		"""Evolve the binary star system in time
 		
 		Uses a Runge-Kutta algorithm to evolve the binary star system over
 		a specified range evolve_over.
 		
 		Arguments:
-			evolve_over {ndarray} -- An ndarray specifying the time that
-			should be integrated over. Each data point in evolve_over
-			corresponds to a datapoint in the output.
+			t_span {tuple} -- A 2-tuple that corresponds to the start and end points of integration.
 		
 		Returns:
 			mixed -- A 3-tuple containing the resultant time array,
 			and the resultant SMA and eccentricity arrays.
 		"""
+
 		def dadt(t, e, a):
 			"""
 			Auxiliary function to compute Equation 3.
@@ -54,7 +55,7 @@ class BinaryStarSystem:
 				The quantity da/dt - how the semimajor axis is changing with time.
 			"""
 			
-			da = (-self.beta / (a**3 * (1-e**2)**(7/2))) * (1 + 73/24 * e**2 + 37 / 96 * e ** 4)
+			da = (-self.beta / (a**3 * (1-e**2)**(7/2))) * (1 + 73/24 * e**2 + 37 / 96 * e ** 4) # Units: km/s
 			
 			return da
 
@@ -72,7 +73,7 @@ class BinaryStarSystem:
 				The quantity de/dt - how the eccentricity is changing with time.
 			"""
 
-			de = (-19/12 * self.beta / (a**4*(1-e**2)**(5/2))) * (e + 121/304 * e ** 3)
+			de = (-19/12 * self.beta / (a**4*(1-e**2)**(5/2))) * (e + 121/304 * e ** 3) # Units: s^-1
  
 			return de
 
@@ -92,6 +93,10 @@ class BinaryStarSystem:
 			e = params[1]
 			return [dadt(t, e, a), dedt(t, e, a)]
 
-		res = solve_ivp(coupled_eqs, (evolve_over[0], evolve_over[-1]), [self.a0, self.e0], t_eval=evolve_over, atol=696.340)
+		evolve_over = np.linspace(t_span[0], t_span[1], 10000)
 
-		return res.t, res.y[0], res.y[1]
+		res = solve_ivp(coupled_eqs, t_span, [self.a0, self.e0], t_eval=evolve_over)
+
+		print(res)
+
+		return res.t / 1e9, np.round(res.y[0] / 696340, 3), np.round(res.y[1], 3)

takahe/BinaryStarSystemLoader.py
@@ -7,7 +7,27 @@ from takahe import BinaryStarSystem
 
 Solar_Mass = 1.989e30
 
-def from_bpass(bpass_from="", mass_fraction=None, data=dict()):
+def from_data(data):
+	"""Loads a binary star system from a dictionary of data.
+	
+	Arguments:
+		data {dict} -- A dictionary containing the 4 elements necessary to solve the 2body problem:
+					   - M1 (mass of primary star)
+					   - M2 (mass of secondary star)
+					   - e0 (current eccentricity)
+					   - a0/T (current semimajor axis / Period)
+					Peroid takes precedence over the SMA, so if one is provided, we use Kepler's law
+					to compute the SMA.
+
+	tw	Returns:
+		BinaryStarSystem -- An ADT to represent the BSS.
+	"""
+	if 'T' in data.keys():
+		data['a0'] = ((G * (data['M1'] + data['M2']))/(4*np.pi**2) * data['T'] ** 2) ** (1/3)
+
+	return BinaryStarSystem.BinaryStarSystem(data['M1'], data['M2'], data['a0'], data['e0'])
+
+def from_bpass(bpass_from, mass_fraction):
 	"""Loads a binary star system from the BPASS dataset.
 	
 	Opens the BPASS file you wish to use, uses hoki to load it into a
@@ -28,30 +48,24 @@ def from_bpass(bpass_from="", mass_fraction=None, data=dict()):
 		[mixed] -- A list of BinaryStarSystem objects (if BPASS data is used).
 				   A singular BinaryStarSystem object (if BPASS data is NOT used).
 	"""
+
+	# Todo: Following code returns a flatline on BPASS data. Why?
+	data = load._stellar_masses(bpass_from)
+
 	if mass_fraction is None:
-		if 'T' in data.keys():
-			data['a0'] = ((G * (data['M1'] + data['M2']))/(4*np.pi**2) * data['T'] ** 2) ** (1/3)
+		raise ValueError
 
-		return BinaryStarSystem.BinaryStarSystem(data['M1'], data['M2'], data['a0'], data['e0'])
+	star_systems = []
 
-	else:
-		# Todo: Following code returns a flatline on BPASS data. Why?
-		data = load._stellar_masses(bpass_from)
+	for mass in data['stellar_mass']:
+		M2 = mass / (2 * mass_fraction)
+		M1 = mass_fraction * M2
 
-		if mass_fraction is None:
-			raise ValueError
+		a0 = np.random.uniform(454368024.65, 454368026.65)
+		e0 = np.random.uniform(0, 1)
 
-		star_systems = []
+		BSS = BinaryStarSystem.BinaryStarSystem(M1, M2, a0, e0)
 
-		for mass in data['stellar_mass']:
-			M2 = mass / (2 * mass_fraction)
-			M1 = mass_fraction * M2
+		star_systems.append(BSS)
 
-			a0 = np.random.uniform(454368024.65, 454368026.65)
-			e0 = np.random.uniform(0, 1)
-
-			BSS = BinaryStarSystem.BinaryStarSystem(M1, M2, a0, e0)
-
-			star_systems.append(BSS)
-
-		return star_systems
+	return star_systems